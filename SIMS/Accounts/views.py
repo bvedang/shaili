0b976267140fde0e9c9935fc6ea2cc9a6e4bca7a
Vedang Barhate <dvedang4u@gmail.com>
2020-02-07 00:10:37 +0530

    Completed Accounts Section

--- SIMS/Accounts/views.py
+++ SIMS/Accounts/views.py
@@ -55,3 +55,56 @@ def cash():
         cash_table = Miscellaneous_table(cash_entries,classes=['table','table-hover'])
         return render_template('accounts_cash.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form,cash_table=cash_table)
     return render_template('accounts_cash.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form)
+
+## Income
+@login_required
+@accounts_home.route('/home/income',methods=['GET','POST'])
+def income():
+    form = Genral_query()
+    amount,date,message=account_value('Income')
+    colors = ['#666547','#fb2e01','#21bf73','#ffcc00','#ffe28a','#f65c78']
+    if form.validate_on_submit():
+        income_entries = Accounts.query.filter(Accounts.date >= form.starting_date.data,Accounts.date <=form.ending_date.data).filter(Accounts.category=='Income').order_by(Accounts.date).limit(10).all()
+        income_table = Miscellaneous_table(income_entries,classes=['table','table-hover'])
+        return render_template('accounts_income.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form,income_table=income_table)
+    return render_template('accounts_income.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form)
+
+
+## Project Payment
+@login_required
+@accounts_home.route('/home/Project_Payment',methods=['GET','POST'])
+def project_payment():
+    form = Genral_query()
+    amount,date,message=account_value('Project Payment')
+    colors = ['#666547','#fb2e01','#21bf73','#ffcc00','#ffe28a','#f65c78']
+    if form.validate_on_submit():
+        project_payment = Accounts.query.filter(Accounts.date >= form.starting_date.data,Accounts.date <=form.ending_date.data).filter(Accounts.category=='Project Payment').order_by(Accounts.date).limit(10).all()
+        project_payment_table = Miscellaneous_table(project_payment,classes=['table','table-hover'])
+        return render_template('accounts_project_payment.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form,project_payment_table=project_payment_table)
+    return render_template('accounts_project_payment.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form)
+
+## Fixed Payment
+@login_required
+@accounts_home.route('/home/Fixed_Payment',methods=['GET','POST'])
+def fixed_payment():
+    form = Genral_query()
+    amount,date,message=account_value('Fixed Payment')
+    colors = ['#666547','#fb2e01','#21bf73','#ffcc00','#ffe28a','#f65c78']
+    if form.validate_on_submit():
+        fixed_payment = Accounts.query.filter(Accounts.date >= form.starting_date.data,Accounts.date <=form.ending_date.data).filter(Accounts.category=='Fixed Payment').order_by(Accounts.date).limit(10).all()
+        fixed_payment_table = Miscellaneous_table(fixed_payment,classes=['table','table-hover'])
+        return render_template('accounts_fixed_payment.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form,fixed_payment_table=fixed_payment_table)
+    return render_template('accounts_fixed_payment.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form)
+
+## Fixed Payment
+@login_required
+@accounts_home.route('/home/Raw_Material',methods=['GET','POST'])
+def raw_material():
+    form = Genral_query()
+    amount,date,message=account_value('Raw Material')
+    colors = ['#666547','#fb2e01','#21bf73','#ffcc00','#ffe28a','#f65c78']
+    if form.validate_on_submit():
+        raw_material = Accounts.query.filter(Accounts.date >= form.starting_date.data,Accounts.date <=form.ending_date.data).filter(Accounts.category=='Raw Material').order_by(Accounts.date).limit(10).all()
+        raw_material_table = Miscellaneous_table(raw_material,classes=['table','table-hover'])
+        return render_template('accounts_raw_material.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form,raw_material_table=raw_material_table)
+    return render_template('accounts_raw_material.html',values = zip(message,amount,date),date=date,amount=amount,colors=colors,form=form)
